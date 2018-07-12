b35b48b8a58116814f7f62de049205bb5b0ffd34
James Schumann <jschumann@gmail.com>
2018-07-12 09:37:15 -0400

    add src core

---
+++ src/Activator.php
@@ -0,0 +1,35 @@
+<?php
+namespace WRDSB\Staff;
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/wrdsb
+ * @since      1.0.0
+ *
+ * @package    WRDSB_Staff
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    WRDSB_Staff
+ * @author     WRDSB <website@wrdsb.ca>
+ */
+class Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ src/Deactivator.php
@@ -0,0 +1,35 @@
+<?php
+namespace WRDSB\Staff;
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/wrdsb
+ * @since      1.0.0
+ *
+ * @package    WRDSB_Staff
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    WRDSB_Staff
+ * @author     WRDSB <website@wrdsb.ca>
+ */
+class Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
