637a847662a81d8a8efd883efa2442f2854f508a
James Schumann <jschumann@gmail.com>
2018-07-12 10:46:41 -0400

    copy boilerplate code to ClassList FrontEnd and BackEnd

---
+++ src/Modules/ClassLists/Views/BackEnd.php
@@ -0,0 +1,82 @@
+<?php
+namespace WRDSB\Staff\Modules\ClassLists;
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * @link       https://github.com/wrdsb
+ * @since      1.0.0
+ *
+ * @package    WRDSB_Staff
+ */
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the admin-specific stylesheet and JavaScript.
+ *
+ * @package    WRDSB_Staff
+ * @author     WRDSB <website@wrdsb.ca>
+ */
+class BackEnd {
+
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $plugin_name    The ID of this plugin.
+	 */
+	private $plugin_name;
+
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $version    The current version of this plugin.
+	 */
+	private $version;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 * @param      string    $plugin_name       The name of this plugin.
+	 * @param      string    $version    The version of this plugin.
+	 */
+	public function __construct( $plugin ) {
+
+		$this->plugin_name = $plugin->get_plugin_name();
+		$this->version     = $plugin->get_version();
+
+		$plugin->add_action( 'admin_enqueue_scripts', $this, 'enqueue_styles' );
+		$plugin->add_action( 'admin_enqueue_scripts', $this, 'enqueue_scripts' );
+
+		add_action( 'admin_menu', array( $this, 'add_admin_page' ) );
+
+	}
+
+	/**
+	 * Register the stylesheets for the admin area.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_styles() {
+
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'assets/css/back-end.css', array(), $this->version, 'all' );
+
+	}
+
+	/**
+	 * Register the JavaScript for the admin area.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_scripts() {
+
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'assets/js/back-end.js', array( 'jquery' ), $this->version, false );
+
+	}
+}
