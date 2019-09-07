924cd8506c0938a6a6148ba990bca345bbb20bc5
James Schumann <jschumann@gmail.com>
2019-09-07 11:20:39 -0400

    conditionally load plugin components if not in admin

--- bootstrap.php
+++ bootstrap.php
@@ -1,13 +1,9 @@
 <?php
 namespace WRDSB\Staff;
 
-use WRDSB\OptionsFramework as OptionsFramework;
-
 use \WRDSB\Staff\Modules\ClassLists\ClassListsModule as ClassListsModule;
 
 use \WRDSB\Staff\Modules\ContentSearch\ContentSearchModule as ContentSearchModule;
-
-use \WRDSB\Staff\Modules\SchoolScheduling\SchoolSchedulingModule as SchoolSchedulingModule;
 
 /**
  * The plugin bootstrap file
@@ -38,65 +34,58 @@ if (! defined('WPINC')) {
     die;
 }
 
-require_once 'vendor/autoload.php';
+if (! is_admin()) {
+    require_once 'vendor/autoload.php';
 
-/**
- * Instantiate the container.
- */
-$container = Plugin::getContainer();
+    /**
+     * Instantiate the container.
+     */
+    $container = Plugin::getContainer();
 
-/**
- * Current plugin name.
- * Change this to your plugin's slug.
- */
-$container['plugin_name'] = 'wrdsb-staff';
+    /**
+     * Current plugin name.
+     * Change this to your plugin's slug.
+     */
+    $container['plugin_name'] = 'wrdsb-staff';
 
-/**
- * Current plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- */
-$container['version'] = '1.0.0';
+    /**
+     * Current plugin version.
+     * Start at version 1.0.0 and use SemVer - https://semver.org
+     */
+    $container['version'] = '1.0.0';
 
-/**
- * Instantiate main plugin class.
- * Pass plugin name and version from container to constructor.
- */
-$container['plugin'] = function ($c) {
-    return new Plugin($c['plugin_name'], $c['version']);
-};
+    /**
+     * Instantiate main plugin class.
+     * Pass plugin name and version from container to constructor.
+     */
+    $container['plugin'] = function ($c) {
+        return new Plugin($c['plugin_name'], $c['version']);
+    };
 
-$container['ClassListsModule'] = function ($c) {
-    return new ClassListsModule($c['plugin']);
-};
+    $container['ClassListsModule'] = function ($c) {
+        return new ClassListsModule($c['plugin']);
+    };
 
-$container['ContentSearchModule'] = function ($c) {
-    return new ContentSearchModule($c['plugin']);
-};
+    $container['ContentSearchModule'] = function ($c) {
+        return new ContentSearchModule($c['plugin']);
+    };
 
-$container['SchoolSchedulingModule'] = function ($c) {
-    return new SchoolSchedulingModule($c['plugin']);
-};
+    /**
+     * Bootstrap the plugin.
+     */
+    $plugin = $container['plugin'];
 
-register_activation_hook(__FILE__, array( __NAMESPACE__ . '\\Activator', 'activate' ));
-register_deactivation_hook(__FILE__, array( __NAMESPACE__ . '\\Deactivator', 'deactivate' ));
+    $schoolCode = get_option('wrdsb_school_code', false);
 
-/**
- * Bootstrap the plugin.
- */
-$plugin = $container['plugin'];
+    $container['ContentSearchModule']->init();
 
-$schoolCode = get_option('wrdsb_school_code', false);
-$schoolSchedulingEnabledFor = ['JAM', 'SSS'];
-$employeeAbsenceEnabledFor = ['JAM', 'SSS'];
+    if ($schoolCode) {
+        $container['ClassListsModule']->init();
+    }
 
-$container['ContentSearchModule']->init();
-
-if ($schoolCode) {
-    $container['ClassListsModule']->init();
+    $plugin->registerHooks();
 }
 
-if ($schoolCode && in_array($schoolCode, $schoolSchedulingEnabledFor)) {
-    $container['SchoolSchedulingModule']->init();
-}
-
-$plugin->registerHooks();
+// Formerly WRDSB Kitchen Sink
+add_filter('send_password_change_email', '__return_false');
+add_filter('send_email_change_email', '__return_false');
