8599ba3869bd53acfc41b19a7bd8afbe6f4a663a
James Schumann <james@schumann.ca>
2021-03-05 14:05:39 -0500

    add theme related functions

--- src/Modules/WP/WPCore.php
+++ src/Modules/WP/WPCore.php
@@ -47,7 +47,7 @@ class WPCore
         return current_user_can($capability, $args);
     }
 
-    public static function wpGetCurrentUser() {
+    public static function getCurrentUser() {
         return wp_get_current_user();
     }
 
@@ -63,7 +63,47 @@ class WPCore
         return restore_current_blog();
     }
 
-    public static function registerRestRoute( string $namespace, string $route, array $args = array(), bool $override = false ): bool {
+    public static function registerRestRoute(string $namespace, string $route, array $args = array(), bool $override = false): bool {
         return register_rest_route($namespace, $route, $args, $override);
+    }
+
+    public static function getOption(string $option, $default = false) {
+        return get_option($option, $default);
+    }
+
+    public static function getCurrentBlogID(): int {
+        return get_current_blog_id();
+    }
+    
+    public static function getFooter() {
+        return get_footer();
+    }
+
+    public static function homeURL() {
+        return home_url();
+    }
+
+    public static function getHeader() {
+        return get_header();
+    }
+
+    public static function getTemplatePart(string $slug, string $name = null, array $args = array()) {
+        return get_template_part($slug, $name, $args);
+    }
+
+    public static function addFilter(string $tag, $function_to_add, int $priority = 10, int $accepted_args = 1) {
+        return add_filter($tag, $function_to_add, $priority, $accepted_args);
+    }
+
+    public static function currentUserCanViewContent() {
+        return current_user_can_view_content();
+    }
+
+    public static function escURL(string $url, $protocols = null, string $_context = 'display'): string {
+        return esc_url($url, $protocols, $_context);
+    }
+
+    public static function adminURL(string $path = '', string $scheme = 'admin'): string {
+        return admin_url($path, $scheme);
     }
 }
