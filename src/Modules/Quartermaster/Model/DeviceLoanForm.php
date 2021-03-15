8ecdf2a01188d432fa456352007be3f77e45cd3d
James Schumann <james@schumann.ca>
2021-03-15 10:31:51 -0400

    fold in changes to supporting files

--- src/Modules/Quartermaster/Model/DeviceLoanForm.php
+++ src/Modules/Quartermaster/Model/DeviceLoanForm.php
@@ -1,11 +1,15 @@
 <?php
 namespace WRDSB\Staff\Modules\Quartermaster\Model;
-
+use WRDSB\Staff\Modules\WP\WPCore as WPCore;
 use WRDSB\Staff\Modules\Quartermaster\Model\DeviceLoanFormCollection as Collection;
 
 use WRDSB\Staff\Modules\Quartermaster\Model\DeviceLoanFormSearch as Search;
 use WRDSB\Staff\Modules\Quartermaster\Model\DeviceLoanFormCommand as Command;
 use WRDSB\Staff\Modules\Quartermaster\Model\DeviceLoanFormQuery as Query;
+
+//use WRDSB\Staff\Modules\Quartermaster\Model\DeviceLoanFormSearch as Search;
+//use WRDSB\Staff\Modules\Quartermaster\Model\DeviceLoanFormCommand as Command;
+//use WRDSB\Staff\Modules\Quartermaster\Model\QuartermasterQuery as Query;
 
 /**
  * Define the "DeviceLoanForm" Model
@@ -17,8 +21,7 @@ use WRDSB\Staff\Modules\Quartermaster\Model\DeviceLoanFormQuery as Query;
  * @subpackage WRDSB_Staff/Quartermaster
  */
 
-class DeviceLoanForm implements \JsonSerializable
-{
+class DeviceLoanForm implements \JsonSerializable {
     private $id;
 
     private $createdAt;
@@ -27,7 +30,7 @@ class DeviceLoanForm implements \JsonSerializable
     private $deleted;
 
     private $saved;
-    //private bool $dirty;
+    private bool $dirty;
 
     private $powerAppsId;
     private $changeDetectionHash;
@@ -36,6 +39,7 @@ class DeviceLoanForm implements \JsonSerializable
     private $submittedAssetID;
     private $correctedAssetID;
     private $deviceType;
+    private $deviceModel;
 
     private $locationName;
     private $locationCode;
@@ -47,6 +51,9 @@ class DeviceLoanForm implements \JsonSerializable
     private $loanedToEmail;
     private $loanedToRole;
     private $receivedBy;
+    private $receivedByRole;
+    private $receivedByName;
+    private $receivedByRelationship;
     
     private $isSEADevice;
     private $addedToSchoolInventory;
@@ -59,12 +66,52 @@ class DeviceLoanForm implements \JsonSerializable
     private $returnedBy;
 
     
-    public function __construct()
-    {
+    public function __construct() {
+        $this->id = '';
+
+        $this->createdAt = '';
+        $this->updatedAt = '';
+        $this->deletedAt = '';
+        $this->deleted = false;
+
+        $this->saved = false;
+        $this->dirty = true;
+
+        $this->powerAppsId = '';
+        $this->changeDetectionHash = '';
+
+        $this->serialNumber = '';
+        $this->submittedAssetID = '';
+        $this->correctedAssetID = '';
+        $this->deviceType = '';
+        $this->deviceModel = '';
+
+        $this->locationName = '';
+        $this->locationCode = '';
+        $this->schoolCode = '';
+
+        $this->loanedBy = '';
+        $this->loanedToName = '';
+        $this->loanedToNumber = '';
+        $this->loanedToEmail = '';
+        $this->loanedToRole = '';
+        $this->receivedBy = '';
+        $this->receivedByRole = '';
+        $this->receivedByName = '';
+        $this->receivedByRelationship = '';
+    
+        $this->isSEADevice = false;
+        $this->addedToSchoolInventory = false;
+        $this->peripheralsProvided = '';
+        $this->timestamp = '';
+        $this->notes = '';
+
+        $this->wasReturned = false;
+        $this->returnedAt = '';
+        $this->returnedBy = '';
     }
 
-    public function jsonSerialize()
-    {
+    public function jsonSerialize() {
         $vars = get_object_vars($this);
         return $vars;
     }
@@ -79,9 +126,65 @@ class DeviceLoanForm implements \JsonSerializable
      * @param string $id The id of the DeviceLoanForm to be returned.
      * @return DeviceLoanForm The DeviceLoanForm whose id matches the id provided.
      */
-    //public static function get(string $id): self
-    //{
-    //}
+    public static function get(string $id): self{
+        $query = new Query('DeviceLoan', $id);
+        $query->run();
+
+        if ($query->getState() === 'success') {
+            $records = $query->getResults();
+            $record = $records[0];
+            $response = new self();
+
+            $response->id = ($record->id) ? $record->id : '';
+            $response->createdAt = ($record->createdAt) ? $record->createdAt : '';
+            $response->updatedAt = ($record->updatedAt) ? $record->updatedAt : '';
+            $response->deletedAt = ($record->deletedAt) ? $record->deletedAt : '';
+            $response->deleted = ($record->deleted) ? $record->deleted : false;
+
+            $response->saved = ($record->saved) ? $record->saved : true;
+            $response->dirty = ($record->dirty) ? $record->dirty : false;
+
+            $response->powerAppsId = ($record->powerAppsId) ? $record->powerAppsId : '';
+            $response->changeDetectionHash = ($record->changeDetectionHash) ? $record->changeDetectionHash : '';
+
+            $response->serialNumber = ($record->serialNumber) ? $record->serialNumber : '';
+            $response->submittedAssetID = ($record->submittedAssetID) ? $record->submittedAssetID : '';
+            $response->correctedAssetID = ($record->correctedAssetID) ? $record->correctedAssetID : '';
+            $response->deviceType = ($record->deviceType) ? $record->deviceType : '';
+            $response->deviceModel = ($record->deviceModel) ? $record->deviceModel : '';
+
+            $response->locationName = ($record->locationName) ? $record->locationName : '';
+            $response->locationCode = ($record->locationCode) ? $record->locationCode : '';
+            $response->schoolCode = ($record->schoolCode) ? $record->schoolCode : '';
+
+            $response->loanedBy = ($record->loanedBy) ? $record->loanedBy : '';
+            $response->loanedToName = ($record->loanedToName) ? $record->loanedToName : '';
+            $response->loanedToNumber = ($record->loanedToNumber) ? $record->loanedToNumber : '';
+            $response->loanedToEmail = ($record->loanedToEmail) ? $record->loanedToEmail : '';
+            $response->loanedToRole = ($record->loanedToRole) ? $record->loanedToRole : '';
+            $response->receivedBy = ($record->receivedBy) ? $record->receivedBy : '';
+            $response->receivedByRole = ($record->receivedByRole) ? $record->receivedByRole : '';
+            $response->receivedByName = ($record->receivedByName) ? $record->receivedByName : '';
+            $response->receivedByRelationship = ($record->receivedByRelationship) ? $record->receivedByRelationship : '';
+
+            $response->isSEADevice = ($record->isSEADevice) ? $record->isSEADevice : false;
+            $response->addedToSchoolInventory = ($record->addedToSchoolInventory) ? $record->addedToSchoolInventory : false;
+            $response->peripheralsProvided = ($record->peripheralsProvided) ? $record->peripheralsProvided : '';
+            $response->timestamp = ($record->timestamp) ? $record->timestamp : '';
+            $response->notes = ($record->notes) ? $record->notes : '';
+
+            $response->wasReturned = ($record->wasReturned) ? $record->wasReturned : false;
+            $response->returnedAt = ($record->returnedAt) ? $record->returnedAt : '';
+            $response->returnedBy = ($record->returnedBy) ? $record->returnedBy : '';
+    
+            return $response;
+
+        } else {
+            error_log($query->getError());
+            $response = new self();
+            return $response;
+        }
+    }
 
     /**
      * Returns the first DeviceLoanForm matching the properties provided.
@@ -201,8 +304,7 @@ class DeviceLoanForm implements \JsonSerializable
      * @param array $args An associative array of property names and their values.
      * @return DeviceLoanForm
      */
-    public static function patch(string $id, array $form): Command
-    {
+    public static function patch(string $id, array $form): Command {
         $command = new Command('patch', $id, $form);
         $command->run();
 
@@ -222,8 +324,7 @@ class DeviceLoanForm implements \JsonSerializable
      *
      * @return boolean
      */
-    public static function delete(string $id): Command
-    {
+    public static function delete(string $id): Command {
         $command = new Command('delete', $id);
         $command->run();
 
@@ -275,8 +376,7 @@ class DeviceLoanForm implements \JsonSerializable
      *
      * @return boolean
      */
-    public function markDeleted(): bool
-    {
+    public function markDeleted(): bool {
         $this->setDeleted();
 
         $command = new Command('delete', $this->id);
@@ -298,8 +398,7 @@ class DeviceLoanForm implements \JsonSerializable
      *
      * @return boolean
      */
-    public function markUndeleted(): bool
-    {
+    public function markUndeleted(): bool {
         $this->setUndeleted();
         
         $command = new Command('patch', $this->id);
@@ -321,8 +420,7 @@ class DeviceLoanForm implements \JsonSerializable
      *
      * @return boolean
      */
-    public function markReturned(): bool
-    {
+    public function markReturned(): bool {
         $this->setReturned();
 
         $command = new Command('patch', $this->id);
@@ -344,8 +442,7 @@ class DeviceLoanForm implements \JsonSerializable
      *
      * @return boolean
      */
-    public function markUnreturned(): bool
-    {
+    public function markUnreturned(): bool {
         $this->setUnreturned();
 
         $command = new Command('patch', $this->id);
@@ -390,7 +487,7 @@ class DeviceLoanForm implements \JsonSerializable
 
     private function setDeleted()
     {
-        $current_time = current_time('mysql');
+        $current_time = WPCore::currentTime();
         $this->deletedAt = $current_time;
         $this->deleted = true;
         //$this->dirty = true;
@@ -405,7 +502,7 @@ class DeviceLoanForm implements \JsonSerializable
 
     private function setReturned()
     {
-        $current_time = current_time('mysql');
+        $current_time = WPCore::currentTime();
         $this->returnedAt = $current_time;
         $this->wasReturned = true;
         $this->returnedBy = '';
@@ -444,31 +541,148 @@ class DeviceLoanForm implements \JsonSerializable
         $this->dirty = $dirty;
     }
 
-    public function setWasReturned(bool $wasReturned)
-    {
+    public function getCreatedAt(): string {
+        return $this->createdAt;
+    }
+
+    public function getUpdatedAt(): string {
+        return $this->updatedAt;
+    }
+
+    public function getDeletedAt(): string {
+        return $this->deletedAt;
+    }
+
+    public function getDeleted(): string {
+        return $this->deleted;
+    }
+
+    public function getSaved(): string {
+        return $this->saved;
+    }
+
+    public function getPowerAppsId(): string {
+        return $this->powerAppsId;
+    }
+
+    public function getChangeDetectionHash(): string {
+        return $this->changeDetectionHash;
+    }
+
+    public function getSerialNumber(): string {
+        return $this->serialNumber;
+    }
+
+    public function getSubmittedAssetID(): string {
+        return $this->submittedAssetID;
+    }
+
+    public function getCorrectedAssetID(): string {
+        return $this->correctedAssetID;
+    }
+
+    public function getDeviceType(): string {
+        return $this->deviceType;
+    }
+
+    public function getDeviceModel(): string {
+        return $this->deviceModel;
+    }
+
+    public function getLocationName(): string {
+        return $this->locationName;
+    }
+
+    public function getLocationCode(): string {
+        return $this->locationCode;
+    }
+
+    public function getSchoolCode(): string {
+        return $this->schoolCode;
+    }
+    public function setSchoolCode(string $schoolCode) {
+        $this->schoolCode = $schoolCode;
+        //$this->dirty = true;
+    }
+
+    public function getLoanedBy(): string {
+        return $this->loanedBy;
+    }
+
+    public function getLoanedToName(): string {
+        return $this->loanedToName;
+    }
+
+    public function getLoanedToNumber(): string {
+        return $this->loanedToNumber;
+    }
+
+    public function getLoanedToEmail(): string {
+        return $this->loanedToEmail;
+    }
+
+    public function getLoanedToRole(): string {
+        return $this->loanedToRole;
+    }
+
+    public function getReceivedBy(): string {
+        return $this->receivedBy;
+    }
+
+    public function getReceivedByRole(): string {
+        return $this->receivedByRole;
+    }
+
+    public function getReceivedByName(): string {
+        return $this->receivedByName;
+    }
+
+    public function getReceivedByRelationship(): string {
+        return $this->receivedByRelationship;
+    }
+
+    public function getIsSEADevice(): string {
+        return $this->isSEADevice;
+    }
+
+    public function getAddedToSchoolInventory(): string {
+        return $this->addedToSchoolInventory;
+    }
+
+    public function getPeripheralsProvided(): string {
+        return $this->peripheralsProvided;
+    }
+
+    public function getTimestamp(): string {
+        return $this->timestamp;
+    }
+
+    public function getNotes(): string {
+        return $this->notes;
+    }
+
+    public function getWasReturned(): string {
+        return $this->wasReturned;
+    }
+    public function setWasReturned(bool $wasReturned) {
         $this->wasReturned= $wasReturned;
         //$this->dirty = true;
     }
 
-    public function setReturnedAt(string $returnedAt)
-    {
+    public function getReturnedAt(): string {
+        return $this->returnedAt;
+    }
+    public function setReturnedAt(string $returnedAt) {
         $this->returnedAt = $returnedAt;
         //$this->dirty = true;
     }
 
-    public function setReturnedBy(string $returnedBy)
-    {
+    public function getReturnedBy(): string {
+        return $this->returnedBy;
+    }
+    public function setReturnedBy(string $returnedBy) {
         $this->returnedBy = $returnedBy;
         //$this->dirty = true;
     }
 
-    public function getSchoolCode(): string
-    {
-        return $this->schoolCode;
-    }
-    public function setSchoolCode(string $schoolCode)
-    {
-        $this->schoolCode = $schoolCode;
-        //$this->dirty = true;
-    }
 }
