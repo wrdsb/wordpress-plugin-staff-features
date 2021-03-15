90a792d62bb567d8093cd9582b94b91a9b4700b6
James Schumann <james@schumann.ca>
2021-03-15 09:25:42 -0400

    version control /vendor for easier deployment

---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'ba3a41d25198ceaaa04ff75a68126d7083aaf757',
+    'name' => 'wrdsb/staff-features',
+  ),
+  'versions' => 
+  array (
+    'pimple/pimple' => 
+    array (
+      'pretty_version' => 'v3.2.3',
+      'version' => '3.2.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9e403941ef9d65d20cba7d54e29fe906db42cf32',
+    ),
+    'psr/container' => 
+    array (
+      'pretty_version' => '1.0.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b7ce3b176482dbbc1245ebf52b181af44c2cf55f',
+    ),
+    'wrdsb/staff-features' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ba3a41d25198ceaaa04ff75a68126d7083aaf757',
+    ),
+  ),
+);
