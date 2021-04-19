2dbc90b6476e7b0091bcd5d129d5348c66c5e816
James Schumann <james@schumann.ca>
2021-04-19 16:47:01 -0400

    fold QuartermasterSearch into QuartermasterQuery

--- src/Modules/Quartermaster/Model/QuartermasterQuery.php
+++ src/Modules/Quartermaster/Model/QuartermasterQuery.php
@@ -17,7 +17,11 @@ use WRDSB\Staff\Modules\Quartermaster\Services\QuartermasterService as Service;
 
 class QuartermasterQuery {
     private $dataType;
-    private $id;
+    private $operation;
+    private $payload;
+
+    private $databaseID;
+    private $searchID;
 
     private $service;
     private $state;
@@ -31,32 +35,82 @@ class QuartermasterQuery {
         return Module::getQuartermasterService();
     }
 
-    public function __construct(string $dataType, string $id) {
+    public function __construct(string $dataType, string $operation, array $params) {
         $this->dataType = $dataType;
-        $this->id = $id;
+        $this->operation = $operation;
+
+        $this->databaseID = $params['databaseID']     ?? null;
+        $this->searchID   = $params['searchID']       ?? null;
+
+        $this->count        = $params['count']        ?? true;
+        $this->facets       = $params['facets']       ?? '';
+        $this->filter       = $params['filter']       ?? '';
+        $this->orderby      = $params['orderby']      ?? '';
+        $this->search       = $params['search']       ?? '*';
+        $this->searchFields = $params['searchFields'] ?? '';
+        $this->select       = $params['select']       ?? '*';
+        $this->skip         = $params['skip']         ?? 0;
+        $this->top          = $params['top']          ?? 50;
+
+        $this->highlight        = $params['highlight']        ?? '';
+        $this->highlightPreTag  = $params['highlightPreTag']  ?? '';
+        $this->highlightPostTag = $params['highlightPostTag'] ?? '';
 
         $this->service = self::getService();
         $this->state   = 'pending';
     }
 
     public function run() {
-        $temp = $this->service->fetch($this);
+        switch ($this->operation) {
+            case 'get':
+                $temp = $this->service->get($this);
 
-        $this->state = $temp->getState();
-        $this->status = $temp->getStatus();
-        $this->rawResponse = $temp->getRawResponse();
-        $this->error = $temp->getError();
+                $this->state = $temp->getState();
+                $this->status = $temp->getStatus();
+                $this->rawResponse = $temp->getRawResponse();
+                $this->error = $temp->getError();
+    
+                if ($this->getState() === 'success') {
+                    $this->results = $this->rawResponse;
+                }
 
-        if ($this->getState() === 'success') {
-            $this->results = $this->rawResponse;
+                break;
+
+            case 'search':
+                $temp = $this->service->search($this);
+
+                $this->state = $temp->getState();
+                $this->status = $temp->getStatus();
+                $this->rawResponse = $temp->getRawResponse();
+                $this->error = $temp->getError();
+    
+                if ($this->getState() === 'success') {
+                    $this->totalResults = $temp->getTotalResults();
+                    $this->results = $temp->getResults();
+                }
+
+                break;
+
+            default:
+                break;
         }
+    }
+
+    public function jsonSerialize() {
+        $vars = get_object_vars($this);
+        return $vars;
     }
 
     public function getDataType(): string {
         return $this->dataType;
     }
-    public function getID(): string {
-        return $this->id;
+
+    public function getOperation(): string {
+        return $this->operation;
+    }
+
+    public function getPayload() {
+        return $this->payload;
     }
 
     public function getState(): string {
