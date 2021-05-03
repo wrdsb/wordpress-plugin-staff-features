1b27d0474fb48284c8493e2c037172db426c33c9
James Schumann <james@schumann.ca>
2021-05-03 09:36:37 -0400

    wordpress auth working as expected now

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,44 +26,38 @@
 
 // See https://stackoverflow.com/questions/63636329/login-to-wordpress-using-cypress-without-using-the-ui
 Cypress.Commands.add("clearWordPressCookies", () => {
-    cy.clearCookie('wordpress_a8b94154380982c3184a469b8aa525c6');
-    cy.clearCookie('wordpress_a8b94154380982c3184a469b8aa525c6');
-    cy.clearCookie('wordpress_logged_in_a8b94154380982c3184a469b8aa525c6');
-    cy.clearCookie('wordpress_test_cookie');
+
+    // Empty defaults
+    Cypress.Cookies.defaults({
+        preserve: []
+    });
+
+    //Clear localStrage
+    cy.clearLocalStorage()
+
+    //Clear Cookies
+    cy.clearCookies()
+
+    // Set defaults
+    Cypress.Cookies.defaults({
+        preserve: /wordpress_.*|woocommerce_.*|wp_woocommerce_.*/
+    })
 });
 
-Cypress.Commands.add("getWordPressCookies", () => {
-    cy.getCookies()
-        .then((cookies) => {
-            cy.writeFile('adminUserLoginCookiesFromCypress.json', cookies);
-        });
-});
-
-Cypress.Commands.add("setWordPressCookies", () => {
-    cy.readFile('adminUserLoginCookiesFromCypress.json')
-        .then((cookies) => {
-            cookies.forEach((cookie) => {
-                // cy.log( JSON.stringify( cookie ) ); // See the cookie contents
-                cy.setCookie(cookie.name, cookie.value, {
-                    domain: Cypress.env('domain'),
-                    path: cookie.path,
-                    secure: cookie.secure,
-                    httpOnly: cookie.httpOnly,
-                    expiry: cookie.expiry
-                });
-            });
-        });
-});
-
+// https://gist.github.com/wpscholar/6a874778c6cab84515b7fa1dceb7a0bf
 Cypress.Commands.add("manualWordPressLogin", () => {
     cy.clearWordPressCookies();
     cy.visit(Cypress.env('dashboardUrl'));
-    cy.get('#user_login').wait(200).type(Cypress.env('users').admin.username, { force: true });
-    cy.get('#user_pass').wait(200).type(Cypress.env('users').admin.pw, { force: true });
-    cy.get('#wp-submit').click();
-    cy.get('h1').contains('Dashboard');
+
+    cy.get('body')
+        .then(($body) => {
+            if ($body.text().includes('Howdy')) {
+                return;
+            } else {
+                cy.get('#user_login').wait(200).type(Cypress.env('users').admin.username, { force: true });
+                cy.get('#user_pass').wait(200).type(Cypress.env('users').admin.pw, { force: true });
+                cy.get('#wp-submit').click();
+                cy.get('h1').contains('Dashboard');
+            }
+        })
 });
-
-
-// https://gist.github.com/wpscholar/6a874778c6cab84515b7fa1dceb7a0bf
-// https://www.lance.bio/2018/05/26/cypress-io-wordpress-login/
