aa3af9d2463903e6f55ffcf1092563a06065b8d5
James Schumann <james@schumann.ca>
2021-08-04 08:49:48 -0400

    add form action

--- src/Modules/SchoolData/Components/EmergencyResponseTeam/Edit.php
+++ src/Modules/SchoolData/Components/EmergencyResponseTeam/Edit.php
@@ -97,6 +97,7 @@ WPCore::addFilter('pre_get_document_title', '\WRDSB\Staff\Modules\SchoolData\Com
 				</div>
 
 				<form id="editEmergencyResponseTeam">
+					<input type="hidden" name="action" value="school_data_emergency_response_team">
                     <input type="hidden" id="blogID" name="blogID" value="<?php echo WPCore::getCurrentBlogID(); ?>">
                     <input type="hidden" id="schoolCode" name="schoolCode" value="<?php echo $schoolCode; ?>">
                     <input type="hidden" id="email" name="email" value="<?php echo $current_user->user_email ?>">
