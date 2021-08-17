27141804669d690b59c7fc9a722012017c7d54b4
James Schumann <james@schumann.ca>
2021-08-17 13:59:03 -0400

    disable form fields in view screens

--- src/Modules/SchoolData/Components/WorkplaceInspectionTeam/View.php
+++ src/Modules/SchoolData/Components/WorkplaceInspectionTeam/View.php
@@ -104,85 +104,85 @@ WPCore::addFilter('pre_get_document_title', '\WRDSB\Staff\Modules\SchoolData\Com
 					<fieldset>
 						<legend>Principal</legend>
 						<label for="principal-firstname" id="label-principal-firstname"><span class="sr-only">Principal </span>Firstname</label>
-						<input type="text" value="<?php echo $currentInstance->getPrincipalFirstname();  ?>" id="principal-firstname" name="principalFirstname" aria-invalid="false" aria-labelledby="label-principal-firstname"><br />
+						<input disabled type="text" value="<?php echo $currentInstance->getPrincipalFirstname();  ?>" id="principal-firstname" name="principalFirstname" aria-invalid="false" aria-labelledby="label-principal-firstname"><br />
 						<label for="principal-lastname" id="label-principal-lastname"><span class="sr-only">Principal </span>Lastname</label>
-						<input type="text" value="<?php echo $currentInstance->getPrincipalLastname();  ?>" id="principal-lastname" name="principalLastname" aria-invalid="false" aria-labelledby="label-principal-lastname"><br />
+						<input disabled type="text" value="<?php echo $currentInstance->getPrincipalLastname();  ?>" id="principal-lastname" name="principalLastname" aria-invalid="false" aria-labelledby="label-principal-lastname"><br />
 						<label for="principal-affiliation" id="label-principal-affiliation"><span class="sr-only">Principal</span> Affiliation</label>
-						<select id="principal-affiliation" name="principalAffiliation" aria-invalid="false" aria-labelledby="label-principal-affiliation">
+						<select disabled id="principal-affiliation" name="principalAffiliation" aria-invalid="false" aria-labelledby="label-principal-affiliation">
 							<option value="<?php echo $currentInstance->getPrincipalAffiliation(); ?>"><?php echo $currentInstance->getPrincipalAffiliation(); ?></option>
 						</select><br />
 						<label for="principal-h-s-contact" id="label-principal-h-s-contact"><span class="sr-only">Principal is </span>H&amp;S Contact?</label>
-						<input id="principal-h-s-contact" name="principalHSContact" type="checkbox" value="1" aria-labelledby="label-principal-h-s-contact">
+						<input disabled id="principal-h-s-contact" name="principalHSContact" type="checkbox" value="1" aria-labelledby="label-principal-h-s-contact">
 					</fieldset>
 
 					<fieldset>
 						<legend>Custodian</legend>
 						<label for="custodian-firstname" id="label-custodian-firstname"><span class="sr-only">Custodian </span>Firstname</label>
-						<input type="text" value="<?php echo $currentInstance->getCustodianFirstname();  ?>" id="custodian-firstname" name="custodianFirstname" aria-invalid="false" aria-labelledby="label-custodian-firstname"><br />
+						<input disabled type="text" value="<?php echo $currentInstance->getCustodianFirstname();  ?>" id="custodian-firstname" name="custodianFirstname" aria-invalid="false" aria-labelledby="label-custodian-firstname"><br />
 						<label for="custodian-lastname" id="label-custodian-lastname"><span class="sr-only">Custodian </span>Lastname</label>
-						<input type="text" value="<?php echo $currentInstance->getCustodianLastname();  ?>" id="custodian-lastname" name="custodianLastname" aria-invalid="false" aria-labelledby="label-custodian-lastname"><br />
+						<input disabled type="text" value="<?php echo $currentInstance->getCustodianLastname();  ?>" id="custodian-lastname" name="custodianLastname" aria-invalid="false" aria-labelledby="label-custodian-lastname"><br />
 						<label for="custodian-affiliation" id="label-custodian-affiliation"><span class="sr-only">Custodian </span>Affiliation</label>
-						<select id="custodian-affiliation" name="custodianAffiliation" aria-invalid="false" aria-labelledby="label-custodian-affiliation">
+						<select disabled id="custodian-affiliation" name="custodianAffiliation" aria-invalid="false" aria-labelledby="label-custodian-affiliation">
 							<option value="<?php echo $currentInstance->getCustodianAffiliation(); ?>"><?php echo $currentInstance->getCustodianAffiliation(); ?></option>
 						</select><br />
 						<label for="custodian-h-s-contact" id="label-custodian-h-s-contact"><span class="sr-only">Custodian is </span>H&amp;S Contact?</span></label>
-						<input id="custodian-h-s-contact" name="custodianHSContact" type="checkbox" value="1" aria-labelledby="label-custodian-h-s-contact">
+						<input disabled id="custodian-h-s-contact" name="custodianHSContact" type="checkbox" value="1" aria-labelledby="label-custodian-h-s-contact">
 					</fieldset>
 
 					<fieldset>
 						<legend>Staff Member 1</legend>
 						<label for="staff-member-1-firstname" id="label-staff-member-1-firstname"><span class="sr-only">Staff Member 1 </span>Firstname</label>
-						<input type="text" value="<?php echo $currentInstance->getStaffMember1Firstname();  ?>" id="staff-member-1-firstname" name="staffMember1Firstname" aria-invalid="false" aria-labelledby="label-staff-member-1-firstname"><br />
+						<input disabled type="text" value="<?php echo $currentInstance->getStaffMember1Firstname();  ?>" id="staff-member-1-firstname" name="staffMember1Firstname" aria-invalid="false" aria-labelledby="label-staff-member-1-firstname"><br />
 						<label for="staff-member-1-lastname" id="label-staff-member-1-lastname"><span class="sr-only">Staff Member 1 </span>Lastname</label>
-						<input type="text" value="<?php echo $currentInstance->getStaffMember1Lastname();  ?>" id="staff-member-1-lastname" name="staffMember1Lastname" aria-invalid="false" aria-labelledby="label-staff-member-1-lastname"><br />
+						<input disabled type="text" value="<?php echo $currentInstance->getStaffMember1Lastname();  ?>" id="staff-member-1-lastname" name="staffMember1Lastname" aria-invalid="false" aria-labelledby="label-staff-member-1-lastname"><br />
 						<label for="staff-member-1-affiliation" id="label-staff-member-1-affiliation"><span class="sr-only">Staff Member 1 </span>Affiliation</label>
-						<select id="staff-member-1-affiliation" name="staffMember1Affiliation" aria-invalid="false" aria-labelledby="label-staff-member-1-affiliation">
+						<select disabled id="staff-member-1-affiliation" name="staffMember1Affiliation" aria-invalid="false" aria-labelledby="label-staff-member-1-affiliation">
 							<option value="<?php echo $currentInstance->getStaffMember1Affiliation(); ?>"><?php echo $currentInstance->getStaffMember1Affiliation(); ?></option>
 						</select><br />
 						<label for="staff-member-1-h-s-contact" id="label-staff-member-1-h-s-contact"><span class="sr-only">Staff Member 1 is</span>H&amp;S Contact?</label>
-						<input id="staff-member-1-h-s-contact" name="staffMember1HSContact" type="checkbox" value="1" aria-labelledby="label-staff-member-1-h-s-contact">
+						<input disabled id="staff-member-1-h-s-contact" name="staffMember1HSContact" type="checkbox" value="1" aria-labelledby="label-staff-member-1-h-s-contact">
 					</fieldset>
 
 					<fieldset>
 						<legend>Staff Member 2</legend>
 						<label for="staff-member-2-firstname" id="label-staff-member-2-firstname"><span class="sr-only">Staff Member 2 </span>Firstname</label>
-						<input type="text" value="<?php echo $currentInstance->getStaffMember2Firstname();  ?>" id="staff-member-2-firstname" name="staffMember2Firstname" aria-invalid="false" aria-labelledby="label-staff-member-2-firstname"><br />
+						<input disabled type="text" value="<?php echo $currentInstance->getStaffMember2Firstname();  ?>" id="staff-member-2-firstname" name="staffMember2Firstname" aria-invalid="false" aria-labelledby="label-staff-member-2-firstname"><br />
 						<label for="staff-member-2-lastname" id="label-staff-member-2-lastname"><span class="sr-only">Staff Member 2 </span>Lastname</label>
-						<input type="text" value="<?php echo $currentInstance->getStaffMember2Lastname();  ?>" id="staff-member-2-lastname" name="staffMember2Lastname" aria-invalid="false" aria-labelledby="label-staff-member-2-lastname"><br />
+						<input disabled type="text" value="<?php echo $currentInstance->getStaffMember2Lastname();  ?>" id="staff-member-2-lastname" name="staffMember2Lastname" aria-invalid="false" aria-labelledby="label-staff-member-2-lastname"><br />
 						<label for="staff-member-2-affiliation" id="label-staff-member-2-affiliation"><span class="sr-only">Staff Member 2 </span>Affiliation</label>
-						<select id="staff-member-2-affiliation" name="staffMember2Affiliation" aria-invalid="false" aria-labelledby="label-staff-member-2-affiliation">
+						<select disabled id="staff-member-2-affiliation" name="staffMember2Affiliation" aria-invalid="false" aria-labelledby="label-staff-member-2-affiliation">
 							<option value="<?php echo $currentInstance->getStaffMember2Affiliation() === ''; ?>"><?php echo $currentInstance->getStaffMember2Affiliation(); ?></option>
 						</select><br />
 						<label for="staff-member-2-h-s-contact" id="label-staff-member-2-h-s-contact"><span class="sr-only">Staff Member 2 is </span>H&amp;S Contact?</label>
-						<input id="staff-member-2-h-s-contact" name="staffMember2HSContact" type="checkbox" value="1" aria-labelledby="label-staff-member-2-h-s-contact">
+						<input disabled id="staff-member-2-h-s-contact" name="staffMember2HSContact" type="checkbox" value="1" aria-labelledby="label-staff-member-2-h-s-contact">
 					</fieldset>
 
 					<fieldset>
 						<legend>Staff Member 3</legend>
 						<label for="staff-member-3-firstname" id="label-staff-member-3-firstname"><span class="sr-only">Staff Member 3 </span>Firstname</label>
-						<input type="text" value="<?php echo $currentInstance->getStaffMember3Firstname();  ?>" id="staff-member-3-firstname" name="staffMember3Firstname" aria-invalid="false" aria-labelledby="label-staff-member-3-firstname"><br />
+						<input disabled type="text" value="<?php echo $currentInstance->getStaffMember3Firstname();  ?>" id="staff-member-3-firstname" name="staffMember3Firstname" aria-invalid="false" aria-labelledby="label-staff-member-3-firstname"><br />
 						<label for="staff-member-3-lastname" id="label-staff-member-3-lastname"><span class="sr-only">Staff Member 3 </span>Lastname</label>
-						<input type="text" value="<?php echo $currentInstance->getStaffMember3Lastname();  ?>" id="staff-member-3-lastname" name="staffMember3Lastname" aria-invalid="false" aria-labelledby="label-staff-member-3-lastname"><br />
+						<input disabled type="text" value="<?php echo $currentInstance->getStaffMember3Lastname();  ?>" id="staff-member-3-lastname" name="staffMember3Lastname" aria-invalid="false" aria-labelledby="label-staff-member-3-lastname"><br />
 						<label for="staff-member-3-affiliation" id="label-staff-member-3-affiliation"><span class="sr-only">Staff Member 3 </span>Affiliation</label>
-						<select id="staff-member-3-affiliation" name="staffMember3Affiliation" aria-invalid="false" aria-labelledby="label-staff-member-3-affiliation">
+						<select disabled id="staff-member-3-affiliation" name="staffMember3Affiliation" aria-invalid="false" aria-labelledby="label-staff-member-3-affiliation">
 							<option value="<?php echo $currentInstance->getStaffMember3Affiliation() === ''; ?>"><?php echo $currentInstance->getStaffMember3Affiliation(); ?></option>
 						</select><br />
 						<label for="staff-member-3-h-s-contact" id="label-staff-member-3-h-s-contact"><span class="sr-only">Staff Member 3 is </span>H&amp;S Contact?</label>
-						<input id="staff-member-3-h-s-contact" name="staffMember3HSContact" type="checkbox" value="1" aria-labelledby="label-staff-member-3-h-s-contact">
+						<input disabled id="staff-member-3-h-s-contact" name="staffMember3HSContact" type="checkbox" value="1" aria-labelledby="label-staff-member-3-h-s-contact">
 					</fieldset>
 
 					<fieldset>
 						<legend>Staff Member 4</legend>
 						<label for="staff-member-4-firstname" id="label-staff-member-4-firstname"><span class="sr-only">Staff Member 4 </span>Firstname</label>
-						<input type="text" value="<?php echo $currentInstance->getStaffMember4Firstname();  ?>" id="staff-member-4-firstname" name="staffMember4Firstname" aria-invalid="false" aria-labelledby="label-staff-member-4-firstname"><br />
+						<input disabled type="text" value="<?php echo $currentInstance->getStaffMember4Firstname();  ?>" id="staff-member-4-firstname" name="staffMember4Firstname" aria-invalid="false" aria-labelledby="label-staff-member-4-firstname"><br />
 						<label for="staff-member-4-lastname" id="label-staff-member-4-lastname"><span class="sr-only">Staff Member 4 <span class="sr-only">Lastname</label>
-						<input type="text" value="<?php echo $currentInstance->getStaffMember4Lastname();  ?>" id="staff-member-4-lastname" name="staffMember4Lastname" aria-invalid="false" aria-labelledby="label-staff-member-4-lastname"><br />
+						<input disabled type="text" value="<?php echo $currentInstance->getStaffMember4Lastname();  ?>" id="staff-member-4-lastname" name="staffMember4Lastname" aria-invalid="false" aria-labelledby="label-staff-member-4-lastname"><br />
 						<label for="staff-member-4-affiliation" id="label-staff-member-4-affiliation"><span class="sr-only"><span class="sr-only">Staff Member 4 </span>Affiliation</label>
-						<select id="staff-member-4-affiliation" name="staffMember4Affiliation" aria-invalid="false" aria-labelledby="label-staff-member-4-affiliation">
+						<select disabled id="staff-member-4-affiliation" name="staffMember4Affiliation" aria-invalid="false" aria-labelledby="label-staff-member-4-affiliation">
 							<option value="<?php echo $currentInstance->getStaffMember4Affiliation() === ''; ?>"><?php echo $currentInstance->getStaffMember4Affiliation(); ?></option>
 						</select><br />
 						<label for="staff-member-4-h-s-contact" id="label-staff-member-4-h-s-contact"><span class="sr-only">Staff Member 4 is </span>H&amp;S Contact?</label>
-						<input id="staff-member-4-h-s-contact" name="staffMember4HSContact" type="checkbox" value="1" aria-labelledby="label-staff-member-4-h-s-contact">
+						<input disabled id="staff-member-4-h-s-contact" name="staffMember4HSContact" type="checkbox" value="1" aria-labelledby="label-staff-member-4-h-s-contact">
 					</fieldset>
 				</form>
 				<!-- /CONTENT -->
