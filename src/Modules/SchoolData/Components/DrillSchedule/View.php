7eb39700406fb2e9b7c2f665024ef85e8f17e646
James Schumann <james@schumann.ca>
2021-08-17 13:46:51 -0400

    wire up current values

--- src/Modules/SchoolData/Components/DrillSchedule/View.php
+++ src/Modules/SchoolData/Components/DrillSchedule/View.php
@@ -102,49 +102,49 @@ WPCore::addFilter('pre_get_document_title', '\WRDSB\Staff\Modules\SchoolData\Com
                     <fieldset>
                         <legend>Fire Drill 1</legend>
                         <label for="fire-drill-1-date" id="label-fire-drill-1-date"><span class="sr-only">Fire Drill 1 </span>Date</label>
-                        <input id="fire-drill-1-date" name="fireDrill1Date" aria-invalid="false" aria-labelledby="label-fire-drill-1-date" type="date" value="">
+                        <input id="fire-drill-1-date" name="fireDrill1Date" aria-invalid="false" aria-labelledby="label-fire-drill-1-date" type="date" value="<?php echo $currentInstance->getFireDrill1Date(); ?>">
                         <label for="fire-drill-1-time" id="label-fire-drill-1-time"><span class="sr-only">Fire Drill 1 </span>Time</label>
-                        <input id="fire-drill-1-time" name="fireDrill1Time" aria-invalid="false" aria-labelledby="label-fire-drill-1-time" type="time" value="">
+                        <input id="fire-drill-1-time" name="fireDrill1Time" aria-invalid="false" aria-labelledby="label-fire-drill-1-time" type="time" value="<?php echo $currentInstance->getFireDrill1Time(); ?>">
                     </fieldset>
 
                     <fieldset>
                         <legend>Fire Drill 2</legend>
                         <label for="fire-drill-2-date" id="label-fire-drill-2-date"><span class="sr-only">Fire Drill 2 </span>Date</label>
-                        <input id="fire-drill-2-date" name="fireDrill2Date" aria-invalid="false" aria-labelledby="label-fire-drill-2-date" type="date" value="">
+                        <input id="fire-drill-2-date" name="fireDrill2Date" aria-invalid="false" aria-labelledby="label-fire-drill-2-date" type="date" value="<?php echo $currentInstance->getFireDrill2Date(); ?>">
                         <label for="fire-drill-2-time" id="label-fire-drill-2-time"><span class="sr-only">Fire Drill 2 </span>Time</label>
-                        <input id="fire-drill-2-time" name="fireDrill2Time" aria-invalid="false" aria-labelledby="label-fire-drill-2-time" type="time" value="">
+                        <input id="fire-drill-2-time" name="fireDrill2Time" aria-invalid="false" aria-labelledby="label-fire-drill-2-time" type="time" value="<?php echo $currentInstance->getFireDrill2Time(); ?>">
                     </fieldset>
 
                     <fieldset>
                         <legend>Fire Drill 3</legend>
                         <label for="fire-drill-3-date" id="label-fire-drill-3-date"><span class="sr-only">Fire Drill 3 </span>Date</label>
-                        <input id="fire-drill-3-date" name="fireDrill3Date" aria-invalid="false" aria-labelledby="label-fire-drill-3-date" type="date" value="">
+                        <input id="fire-drill-3-date" name="fireDrill3Date" aria-invalid="false" aria-labelledby="label-fire-drill-3-date" type="date" value="<?php echo $currentInstance->getFireDrill3Date(); ?>">
                         <label for="fire-drill-3-time" id="label-fire-drill-3-time"><span class="sr-only">Fire Drill 3 </span>Time</label>
-                        <input id="fire-drill-3-time" name="fireDrill3Time" aria-invalid="false" aria-labelledby="label-fire-drill-3-time" type="time" value="">
+                        <input id="fire-drill-3-time" name="fireDrill3Time" aria-invalid="false" aria-labelledby="label-fire-drill-3-time" type="time" value="<?php echo $currentInstance->getFireDrill3Time(); ?>">
                     </fieldset>
 
                     <fieldset>
                         <legend>Fire Drill 4</legend>
                         <label for="fire-drill-4-date" id="label-fire-drill-4-date"><span class="sr-only">Fire Drill 4 </span>Date</label>
-                        <input id="fire-drill-4-date" name="fireDrill4Date" aria-invalid="false" aria-labelledby="label-fire-drill-4-date" type="date" value="">
+                        <input id="fire-drill-4-date" name="fireDrill4Date" aria-invalid="false" aria-labelledby="label-fire-drill-4-date" type="date" value="<?php echo $currentInstance->getFireDrill4Date(); ?>">
                         <label for="fire-drill-4-time" id="label-fire-drill-4-time"><span class="sr-only">Fire Drill 4 </span>Time</label>
-                        <input id="fire-drill-4-time" name="fireDrill4Time" aria-invalid="false" aria-labelledby="label-fire-drill-4-time" type="time" value="">
+                        <input id="fire-drill-4-time" name="fireDrill4Time" aria-invalid="false" aria-labelledby="label-fire-drill-4-time" type="time" value="<?php echo $currentInstance->getFireDrill4Time(); ?>">
                     </fieldset>
 
                     <fieldset>
                         <legend>Fire Drill 5</legend>
                         <label for="fire-drill-5-date" id="label-fire-drill-5-date"><span class="sr-only">Fire Drill 5 </span>Date</label>
-                        <input id="fire-drill-5-date" name="fireDrill5Date" aria-invalid="false" aria-labelledby="label-fire-drill-5-date" type="date" value="">
+                        <input id="fire-drill-5-date" name="fireDrill5Date" aria-invalid="false" aria-labelledby="label-fire-drill-5-date" type="date" value="<?php echo $currentInstance->getFireDrill5Date(); ?>">
                         <label for="fire-drill-5-time" id="label-fire-drill-5-time"><span class="sr-only">Fire Drill 5 </span>Time</label>
-                        <input id="fire-drill-5-time" name="fireDrill5Time" aria-invalid="false" aria-labelledby="label-fire-drill-5-time" type="time" value="">
+                        <input id="fire-drill-5-time" name="fireDrill5Time" aria-invalid="false" aria-labelledby="label-fire-drill-5-time" type="time" value="<?php echo $currentInstance->getFireDrill5Time(); ?>">
                     </fieldset>
 
                     <fieldset>
                         <legend>Fire/Bomb Drill</legend>
                         <label for="bomb-drill-date" id="label-bomb-drill-date"><span class="sr-only">Fire/Bomb Drill </span>Date</label>
-                        <input id="bomb-drill-date" name="bombDrillDate" aria-invalid="false" aria-labelledby="label-bomb-drill-date" type="date" value="">
+                        <input id="bomb-drill-date" name="bombDrillDate" aria-invalid="false" aria-labelledby="label-bomb-drill-date" type="date" value="<?php echo $currentInstance->getBombDrill1Date(); ?>">
                         <label for="bomb-drill-time" id="label-bomb-drill-time"><span class="sr-only">Fire/Bomb Drill </span>Time</label>
-                        <input id="bomb-drill-time" name="bombDrillTime" aria-invalid="false" aria-labelledby="label-bomb-drill-time" type="time" value="">
+                        <input id="bomb-drill-time" name="bombDrillTime" aria-invalid="false" aria-labelledby="label-bomb-drill-time" type="time" value="<?php echo $currentInstance->getBombDrill1Time(); ?>">
                     </fieldset>
                 </form>
                 <!-- /CONTENT -->
