ff978caffa0a22e34c31aa380732c0c1f1324e07
James Schumann <james@schumann.ca>
2021-09-09 15:43:37 -0400

    go full width; update breadcrumbs for admin home page

--- src/Modules/SchoolData/Components/List/DrillSchedule.php
+++ src/Modules/SchoolData/Components/List/DrillSchedule.php
@@ -31,7 +31,7 @@ WPCore::addFilter('pre_get_document_title', '\WRDSB\Staff\Modules\SchoolData\Com
                     <a href="<?php echo WPCore::homeURL(); ?>/school-data/home">School Data</a>
                 </li>
                 <li>
-                    <a href="..">List</a>
+                    <a href="<?php echo WPCore::homeURL(); ?>/school-data/admin">Admin</a>
                 </li>
                 <li>
                     Drill Schedule
@@ -47,38 +47,7 @@ WPCore::addFilter('pre_get_document_title', '\WRDSB\Staff\Modules\SchoolData\Com
             <?php //echo PermissionDenied::cannotView(); ?>
 
         <?php } else { ?>
-            <div class="col-sm-3 col-lg-3" role="complementary">
-                <div class="navbar my-sub-navbar" id="section_navigation" role="navigation">
-                    <div class="sub-navbar-header">
-                        <button type="button" class="navbar-toggle toggle-subnav" data-toggle="collapse" data-target=".sub-navbar-collapse">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <span class="navbar-brand">Subnav</span>
-                    </div>
-                    <div class="collapse sub-navbar-collapse">
-                        <div class="sub-menu-heading">
-                            <span><a href="<?php echo WPCore::homeURL(); ?>/school-data/home">School Data</a></span>
-                        </div>
-                        <div class="sub-menu-items">
-                            <ul>
-                                <ul>
-                                    <li><a href="<?php echo WPCore::homeURL(); ?>/school-data/drill-schedule/">Drill Schedule</a></li>
-                                    <li><a href="<?php echo WPCore::homeURL(); ?>/school-data/emergency-response-team/">Emergency Response Team</a></li>
-                                    <li><a href="<?php echo WPCore::homeURL(); ?>/school-data/evacuation-sites/">Evacuation Sites</a></li>
-                                    <li><a href="<?php echo WPCore::homeURL(); ?>/school-data/iprc/">IPRC</a></li>
-                                    <li><a href="<?php echo WPCore::homeURL(); ?>/school-data/scis-team">SCIS Team</a></li>
-                                    <li><a href="<?php echo WPCore::homeURL(); ?>/school-data/workplace-inspection-team/">Workplace Inspection Team</a></li>
-                                </ul>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-9 col-lg-9" role="main">
+            <div class="col-sm-12 col-lg-12" role="main">
                 <!-- CONTENT -->
                 <h1><?php echo $page_title; ?></h1>
 
