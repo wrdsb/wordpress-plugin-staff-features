5d434ec62e7bfcca184b6e485fc4b8ad5b3e25be
James Schumann <james@schumann.ca>
2021-09-13 13:37:44 -0400

    create audit views for remaining CPTs

---
+++ src/Modules/SchoolData/Components/Search/Audit/EmergencyResponseTeam.php
@@ -0,0 +1,137 @@
+<?php
+namespace WRDSB\Staff\Modules\SchoolData\Components;
+
+use WRDSB\Staff\Modules\WP\WPCore as WPCore;
+use WRDSB\Staff\Modules\SchoolData\Model\SchoolsList as SchoolsList;
+use WRDSB\Staff\Modules\SchoolData\Model\PrincipalsList as PrincipalsList;
+use WRDSB\Staff\Modules\SchoolData\Model\EmergencyResponseTeamSearch as EmergencyResponseTeamSearch;
+
+$page_title = "Emergency Response Team Audit";
+
+function setCustomTitle() {
+    $page_title = "Emergency Response Team Audit";
+    return $page_title;
+}
+WPCore::addFilter('pre_get_document_title', '\WRDSB\Staff\Modules\SchoolData\Components\setCustomTitle');
+
+$schools = SchoolsList::all();
+$principals = PrincipalsList::all();
+$audit = EmergencyResponseTeamSearch::audit();
+?>
+
+<?php WPCore::getHeader(); ?>
+
+<div class="container-top">
+    <?php WPCore::getTemplatePart('partials/header', 'masthead'); ?>
+
+    <?php if (! WPCore::currentUserCanViewContent()) { ?>
+        <?php WPCore::getTemplatePart('partials/content', 'unauthorized'); ?>
+    <?php } else { ?>
+        <?php WPCore::getTemplatePart('partials/header', 'navbar'); ?>
+
+        <div class="container container-breadcrumb" role="navigation">
+            <ol class="breadcrumb">
+                <li>
+                    <a href="<?php echo WPCore::getOption('home'); ?>">Home</a>
+                </li>
+                <li>
+                    <a href="<?php echo WPCore::homeURL(); ?>/school-data/home">School Data</a>
+                </li>
+                <li>
+                    <a href="<?php echo WPCore::homeURL(); ?>/school-data/admin">Admin</a>
+                </li>
+                <li>
+                    Emergency Response Team
+                </li>
+            </ol>
+        </div>
+    <?php } ?>
+</div>
+
+<div class="container">
+    <div class="row">
+        <?php if (!WPCore::currentUserCanViewContent()) { ?>
+            <?php //echo PermissionDenied::cannotView(); ?>
+
+        <?php } else { ?>
+            <div class="col-sm-12 col-lg-12" role="main">
+                <!-- CONTENT -->
+                <h1><?php echo $page_title; ?></h1>
+
+                <table id="sample-data-table" class="table">
+                    <thead>
+                        <tr>
+                            <th class="secondary-text">
+                                <div class="table-header">
+                                    <span class="column-title">Code</span>
+                                </div>
+                            </th>
+                            <th class="secondary-text">
+                                <div class="table-header">
+                                    <span class="column-title">Name</span>
+                                </div>
+                            </th>
+                            <th class="secondary-text">
+                                <div class="table-header">
+                                    <span class="column-title">Last Update</span>
+                                </div>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($audit['good'] as $schoolCode => $post) { ?>
+                            <tr>
+                                <td><a href="<?php echo WPCore::homeURL(); ?>/school-data/single/emergency-response-team/<?php echo strtolower($post->schoolCode); ?>/"><?php echo strtoupper($post->schoolCode); ?></a></td>
+                                <td><?php echo $schools[strtolower($schoolCode)]; ?></td>
+                                <td><?php echo $post->post_modified; ?></td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+
+                <table id="sample-data-table" class="table">
+                    <thead>
+                        <tr>
+                            <th class="secondary-text">
+                                <div class="table-header">
+                                    <span class="column-title">Code</span>
+                                </div>
+                            </th>
+                            <th class="secondary-text">
+                                <div class="table-header">
+                                    <span class="column-title">Name</span>
+                                </div>
+                            </th>
+                            <th class="secondary-text">
+                                <div class="table-header">
+                                    <span class="column-title">Principal</span>
+                                </div>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($audit['bad'] as $schoolCode => $schoolName) { ?>
+                            <tr>
+                                <td><?php echo strtoupper($schoolCode); ?></td>
+                                <td><?php echo $schoolName; ?></td>
+                                <td><?php echo $principals[strtolower($schoolCode)]; ?></td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+                <!-- /CONTENT -->
+            </div>
+        <?php } ?>
+    </div>
+</div>
+
+<script>
+    function disable(id) {
+        document.getElementById(id).disabled = true;
+    }
+    function enable(id) {
+        document.getElementById(id).disabled = false;
+    }
+</script>
+
+<?php WPCore::getFooter();
